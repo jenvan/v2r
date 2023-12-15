6f8990db5df04e73572f409c5e121df69ef01a0d
jenvan <jenvan@qq.com>
2023-12-15 21:51:05 +0800

    修改 Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,22 @@
 FROM nginx:mainline-alpine-slim
-EXPOSE 80
 USER root
+
+EXPOSE 80
 
 ENV HOST test.com
 ENV UUID de04add9-1234-abcd-950c-08cd5320df18
 ENV VMESS_WSPATH /vmess
 ENV VLESS_WSPATH /vless
 
-RUN mkdir /etc/v2ray /usr/local/v2ray
+RUN mkdir /usr/local/v2ray /etc/v2ray
 
+COPY v2ray /usr/local/v2ray/
+COPY config.json /etc/v2ray/
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY config.html /usr/share/nginx/html/config.html
-COPY config.json /etc/v2ray/
-COPY v2ray /usr/local/v2ray/
 COPY entrypoint.sh /entrypoint.sh
 
-RUN  chmod a+x /usr/local/v2ray/v2ray && \
-     chmod a+x /entrypoint.sh
+RUN  chmod +x /usr/local/v2ray/v2ray && \
+     chmod +x /entrypoint.sh
     
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,10 @@ VLESS_WSPATH=${VLESS_WSPATH:-'/vl'}
 VMESS_LINK=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"bing.com\",\"add\":\"$HOST\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$HOST\",\"path\":\"$VMESS_WSPATH\",\"tls\":\"tls\"}" | base64 -w 0)
 VLESS_LINK=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$HOST":443?encryption=none&security=tls&type=ws&host="$HOST"&path="$VLESS_WSPATH"#bing.com"
 
-sed -i "s#VMESS_LINK#$VMESS_LINK#g;s#VLESS_LINK#$VLESS_LINK#g" /usr/share/nginx/html/config.html
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
+sed -i "s|VMESS_LINK|$VMESS_LINK|g;s|VLESS_LINK|$VLESS_LINK|g" /usr/share/nginx/html/config.html
+sed -i "s|UUID|$UUID|g;s|VMESS_WSPATH|$VMESS_WSPATH|g;s|VLESS_WSPATH|$VLESS_WSPATH|g" /etc/v2ray/config.json
+sed -i "s|UUID|$UUID|g;s|VMESS_WSPATH|$VMESS_WSPATH|g;s|VLESS_WSPATH|$VLESS_WSPATH|g" /etc/nginx/nginx.conf
 
 nginx
+
 /usr/local/v2ray/v2ray run -config=/etc/v2ray/config.json
